44833849b7b6639785277989c8f7b6fd12a9169d
olin4is <151752844+olin4is@users.noreply.github.com>
2025-03-23 18:30:28 +0500

    Create utils

---
+++ utils.py
@@ -0,0 +1,32 @@
+def round_rectangle(canvas, x1, y1, x2, y2, radius=25, **kwargs):
+    points = [x1+radius, y1,
+              x1+radius, y1,
+              x2-radius, y1,
+              x2-radius, y1,
+              x2, y1,
+              x2, y1+radius,
+              x2, y1+radius,
+              x2, y2-radius,
+              x2, y2-radius,
+              x2, y2,
+              x2-radius, y2,
+              x2-radius, y2,
+              x1+radius, y2,
+              x1+radius, y2,
+              x1, y2,
+              x1, y2-radius,
+              x1, y2-radius,
+              x1, y1+radius,
+              x1, y1+radius,
+              x1, y1]
+
+    return canvas.create_polygon(points, **kwargs, smooth=True)
+
+def change_on_hover(button):
+    colorOnHover = "#445128"
+    colorOnLeave = "#606c38"
+    button.bind("<Enter>", func=lambda e: button.config(
+        background=colorOnHover))
+ 
+    button.bind("<Leave>", func=lambda e: button.config(
+        background=colorOnLeave))
