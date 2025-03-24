a0bffb88b8e2e237a4a6b3c9506881298c9a1041
olin4is <151752844+olin4is@users.noreply.github.com>
2025-03-24 21:00:55 +0500

    Update window size

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from questions import data
 from education_quiz import Education_Quiz
 
 window = Tk()
-window.geometry("800x500")
+window.minsize(800, 500)
+window.maxsize(800, 500)
 window.title("Пожарная безопасность. Учебный тренажер")
 
 quiz = Education_Quiz(window, data)
